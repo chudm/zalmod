9cf6ccabd645595b34d57e09c78f7ad6f46d4c1b
chudm <jjcastillomejias@gmail.com>
2026-03-08 17:38:38 -0300

    Update models.dart

--- zal_app/lib/Functions/models.dart
+++ zal_app/lib/Functions/models.dart
@@ -9,7 +9,6 @@ import 'package:intl/intl.dart';
 import 'package:socket_io_client/socket_io_client.dart';
 import 'package:zal/Functions/utils.dart';
 
-// --- ENUMS ---
 enum AmdOrNvidia { amd, nvidia }
 enum RyzenOrIntel { ryzen, intel }
 enum ProgramTimesTimeframe { today, yesterday }
@@ -25,7 +24,6 @@ enum FileProviderState { downloading, rebuilding, complete }
 enum MoveFileType { move, copy }
 enum SortFilesBy { nameAscending, nameDescending, sizeAscending, sizeDescending, dateModifiedAscending, dateModifiedDescending, dateCreatedAscending, dateCreatedDescending }
 
-// --- HELPER CLASSES ---
 class ComputerAddress {
   String name;
   String ip;
@@ -71,8 +69,6 @@ class FileData {
   factory FileData.fromMap(Map<String, dynamic> map) => FileData(name: map['name'] ?? '', extension: map['extension'], directory: map['directory'] ?? '', size: map['size']?.toInt() ?? 0, fileType: FileType.values.byName(map['fileType']), dateCreated: map['dateCreated'] != null ? DateTime.fromMillisecondsSinceEpoch(map['dateCreated']) : DateTime.fromMillisecondsSinceEpoch(0), dateModified: map['dateModified'] != null ? DateTime.fromMillisecondsSinceEpoch(map['dateModified']) : DateTime.fromMillisecondsSinceEpoch(0));
 }
 
-// --- CORE HARDWARE MODELS (UPGRADED) ---
-
 class Cpu {
   String name;
   double? temperature;
@@ -94,7 +90,6 @@ class Cpu {
       }
     }
 
-    // UPGRADE: Hunt for Zen 5 (9700X) temperature sensors if standard is missing
     double? temp = map['temperature']?.toDouble();
     if (temp == null || temp <= 0.1) {
       map.forEach((key, value) {
@@ -105,7 +100,7 @@ class Cpu {
       });
     }
 
-    final cpu = Cpu(
+    return Cpu(
       name: map['name'] ?? '',
       temperature: temp,
       power: map['power']?.toDouble() ?? 0.0,
@@ -116,7 +111,6 @@ class Cpu {
       voltages: SplayTreeMap<String, double>.from(map['voltages'] ?? {}),
       cpuInfo: map['cpuInfo'] != null ? CpuInfo.fromMap(map['cpuInfo']) : null,
     );
-    return cpu;
   }
 
   factory Cpu.nullData() => Cpu(name: "-1", temperature: -1, power: -1, powers: SplayTreeMap(), clocks: SplayTreeMap(), load: -1, loads: SplayTreeMap(), voltages: SplayTreeMap(), cpuInfo: null);
@@ -137,7 +131,6 @@ class Gpu {
   Gpu({required this.name, required this.coreSpeed, required this.memorySpeed, required this.fanSpeedPercentage, required this.corePercentage, required this.power, required this.dedicatedMemoryUsed, required this.temperature, required this.voltage, required this.fps});
 
   factory Gpu.fromMap(Map<String, dynamic> map) {
-    // UPGRADE: Fix for the NVIDIA 50-series "255 degree" overflow bug
     double temp = map['temperature']?.toDouble() ?? 0.0;
     if (temp >= 250 || temp <= 0) {
       map.forEach((key, value) {
@@ -161,4 +154,251 @@ class Gpu {
     );
   }
 
-  factory Gpu.nullData() => Gpu(name: "-1", coreSpeed: -1, memorySpeed: -
+  factory Gpu.nullData() => Gpu(name: "-1", coreSpeed: -1, memorySpeed: -1, fanSpeedPercentage: -1, corePercentage: -1, power: -1, dedicatedMemoryUsed: -1, temperature: -1, voltage: -1, fps: -1);
+}
+
+class ComputerData {
+  late Map<String, dynamic> rawData;
+  late Ram ram;
+  late Cpu cpu;
+  late Gpu gpu;
+  late List<Storage> storages;
+  late List<Monitor> monitors;
+  late Motherboard motherboard;
+  late Battery battery;
+  late List<NetworkInterface> networkInterfaces;
+  List<String>? availableGpus;
+  List<TaskmanagerProcess>? taskmanagerProcesses;
+  NetworkSpeed? networkSpeed;
+  late bool isRunningAsAdminstrator;
+  late Map<String, List<dynamic>> charts;
+  Map<int, double>? processesGpuUsage = {};
+
+  ComputerData();
+
+  ComputerData.construct(dynamic data) {
+    Map<String, dynamic> parsedData;
+    try {
+      if (data is String) {
+        parsedData = jsonDecode(data);
+      } else if (data is Map) {
+        parsedData = Map<String, dynamic>.from(data);
+      } else {
+        parsedData = jsonDecode(data.toString().replaceAll("'", '"'));
+      }
+    } catch (e) {
+      parsedData = jsonDecode(data.toString().replaceAll("'", '"'));
+    }
+
+    rawData = parsedData;
+    availableGpus = parsedData.containsKey("availableGpus") ? List<String>.from(parsedData['availableGpus']) : [];
+    charts = parsedData.containsKey("charts") ? Map<String, List<dynamic>>.from(parsedData['charts']) : {};
+    
+    final computerData = parsedData['computerData'] ?? parsedData;
+    isRunningAsAdminstrator = computerData['isAdminstrator'] ?? false;
+    
+    ram = computerData['ramData'] != null ? Ram.fromMap(computerData['ramData']) : Ram.nullData();
+    cpu = computerData['cpuData'] != null ? Cpu.fromMap(computerData['cpuData']) : Cpu.nullData();
+    gpu = computerData['gpuData'] != null ? Gpu.fromMap(computerData['gpuData']) : Gpu.nullData();
+    motherboard = computerData['motherboardData'] != null ? Motherboard.fromMap(computerData['motherboardData']) : Motherboard.nullData();
+    battery = computerData['batteryData'] != null ? Battery.fromMap(computerData['batteryData']) : Battery.nullData();
+    
+    if (computerData['storagesData'] != null) {
+      storages = List<Map<String, dynamic>>.from(computerData['storagesData']).map((e) => Storage.fromMap(e)).toList();
+      storages.sort((a, b) => a.diskNumber.compareTo(b.diskNumber));
+    } else {
+      storages = [Storage.nullData()];
+    }
+
+    if (computerData['monitorsData'] != null) {
+      monitors = List<Map<String, dynamic>>.from(computerData['monitorsData']).map((e) => Monitor.fromMap(e)).toList();
+    } else {
+      monitors = [Monitor.nullData()];
+    }
+    
+    if (computerData['primaryNetworkSpeed'] != null) networkSpeed = NetworkSpeed.fromMap(computerData["primaryNetworkSpeed"]);
+    if (computerData.containsKey("networkInterfaces")) {
+      networkInterfaces = List<Map<String, dynamic>>.from(computerData['networkInterfaces']).map((e) => NetworkInterface.fromMap(e)).toList();
+    }
+    if (computerData.containsKey("taskmanagerData") && computerData['taskmanagerData'] != null) {
+      taskmanagerProcesses = Map<String, dynamic>.from(computerData['taskmanagerData']).entries.map((e) => TaskmanagerProcess.fromMap(e)).toList();
+    }
+  }
+}
+
+class Ram {
+  final double memoryUsed;
+  final double memoryAvailable;
+  final int memoryUsedPercentage;
+  final List<RamPiece> ramPieces;
+  Ram({required this.memoryUsed, required this.memoryAvailable, required this.memoryUsedPercentage, required this.ramPieces});
+  factory Ram.fromMap(Map<String, dynamic> map) => Ram(memoryUsed: map['memoryUsed']?.toDouble() ?? 0, memoryAvailable: map['memoryAvailable']?.toDouble() ?? 0, memoryUsedPercentage: map['memoryUsedPercentage']?.toInt() ?? 0, ramPieces: map['ramPiecesData'] != null ? List<RamPiece>.from(map['ramPiecesData']?.map((x) => RamPiece.fromMap(x))) : []);
+  factory Ram.nullData() => Ram(memoryUsed: -1, memoryAvailable: -1, memoryUsedPercentage: -1, ramPieces: []);
+}
+
+class RamPiece {
+  final int capacity;
+  final String manufacturer;
+  final String partNumber;
+  final int clockSpeed;
+  RamPiece({required this.capacity, required this.manufacturer, required this.partNumber, required this.clockSpeed});
+  factory RamPiece.fromMap(Map<String, dynamic> map) => RamPiece(capacity: map['capacity']?.toInt() ?? 0, manufacturer: map['manufacturer'] ?? '', partNumber: map['partNumber'] ?? '', clockSpeed: map['speed']?.toInt() ?? 0);
+}
+
+class Storage {
+  final int diskNumber;
+  final int totalSize;
+  final int freeSpace;
+  final int temperature;
+  final int readRate;
+  final int writeRate;
+  final String type;
+  final List<Partition>? partitions;
+  final List<SmartAttribute> smartAttributes;
+  Storage({required this.diskNumber, required this.totalSize, required this.freeSpace, required this.temperature, required this.readRate, required this.writeRate, required this.type, required this.partitions, required this.smartAttributes});
+  factory Storage.fromMap(Map<String, dynamic> map) {
+    List<Partition>? partitions;
+    if (map.containsKey("partitions") && map['partitions'] != null) {
+      partitions = List<Partition>.from(map['partitions']?.map((x) => Partition.fromMap(x)))..sort((b, a) => a.size.compareTo(b.size));
+    }
+    return Storage(diskNumber: map['diskNumber']?.toInt() ?? 0, totalSize: map['totalSize']?.toInt() ?? 0, freeSpace: map['freeSpace']?.toInt() ?? 0, temperature: map['temperature']?.toInt() ?? 0, readRate: map['readRate']?.toInt() ?? 0, writeRate: map['writeRate']?.toInt() ?? 0, type: map['type'] ?? '', partitions: partitions, smartAttributes: map['smartAttributes'] != null ? List<Map<String, dynamic>>.from(map['smartAttributes']).map((e) => SmartAttribute.fromMap(e)).toList() : []);
+  }
+  factory Storage.nullData() => Storage(diskNumber: -1, totalSize: -1, freeSpace: -1, temperature: -1, readRate: -1, writeRate: -1, type: "HDD", partitions: null, smartAttributes: []);
+}
+
+class Partition {
+  final String driveLetter;
+  final String label;
+  final int size;
+  final int freeSpace;
+  Partition({required this.driveLetter, required this.label, required this.size, required this.freeSpace});
+  factory Partition.fromMap(Map<String, dynamic> map) => Partition(driveLetter: map['driveLetter'] ?? '', label: map['label'] ?? '', size: map['size']?.toInt() ?? 0, freeSpace: map['freeSpace']?.toInt() ?? 0);
+}
+
+class SmartAttribute {
+  final String attributeName;
+  final int id;
+  final int rawValue;
+  SmartAttribute({required this.attributeName, required this.id, required this.rawValue});
+  factory SmartAttribute.fromMap(Map<String, dynamic> map) => SmartAttribute(attributeName: map['attributeName'] ?? '', id: int.tryParse(map['id'].toString()) ?? 0, rawValue: map['rawValue']?.toInt() ?? 0);
+}
+
+class Motherboard {
+  String name;
+  double temperature;
+  Motherboard({required this.name, required this.temperature});
+  factory Motherboard.fromMap(Map<String, dynamic> map) => Motherboard(name: map['name'] ?? '', temperature: map['temperature']?.toDouble() ?? 0.0);
+  factory Motherboard.nullData() => Motherboard(name: '-1', temperature: -1);
+}
+
+class Monitor {
+  String name;
+  bool primary;
+  int height;
+  int width;
+  Monitor({required this.name, required this.primary, required this.height, required this.width});
+  factory Monitor.fromMap(Map<String, dynamic> map) => Monitor(name: map['name'] ?? '', primary: map['primary'] ?? false, height: map['height']?.toInt() ?? 0, width: map['width']?.toInt() ?? 0);
+  factory Monitor.nullData() => Monitor(name: '-1', primary: false, height: -1, width: -1);
+}
+
+class Battery {
+  bool isCharging;
+  int batteryPercentage;
+  int lifeRemaining;
+  bool hasBattery;
+  Battery({required this.isCharging, required this.batteryPercentage, required this.lifeRemaining, required this.hasBattery});
+  factory Battery.fromMap(Map<String, dynamic> map) => Battery(isCharging: map['isCharging'] ?? false, batteryPercentage: map['life']?.toInt() ?? 0, lifeRemaining: map['lifeRemaining']?.toInt() ?? 0, hasBattery: map['hasBattery'] ?? false);
+  factory Battery.nullData() => Battery(isCharging: false, batteryPercentage: 0, lifeRemaining: 0, hasBattery: false);
+}
+
+class NetworkSpeed {
+  final int download;
+  final int upload;
+  NetworkSpeed({required this.download, required this.upload});
+  factory NetworkSpeed.fromMap(Map<String, dynamic> map) => NetworkSpeed(download: map['download']?.toInt() ?? 0, upload: map['upload']?.toInt() ?? 0);
+}
+
+class NetworkInterface {
+  final String name;
+  final String description;
+  final bool isEnabled;
+  final int bytesSent;
+  final int bytesReceived;
+  NetworkInterface({required this.name, required this.description, required this.isEnabled, required this.bytesSent, required this.bytesReceived});
+  factory NetworkInterface.fromMap(Map<String, dynamic> map) => NetworkInterface(name: map['name'] ?? '', description: map['description'] ?? '', isEnabled: (map['status'] ?? "Down") != "Down", bytesSent: map['bytesSent']?.toInt() ?? 0, bytesReceived: map['bytesReceived']?.toInt() ?? 0);
+}
+
+class TaskmanagerProcess {
+  List<int> pids;
+  String name;
+  double memoryUsage;
+  double cpuPercent;
+  Uint8List? icon;
+  TaskmanagerProcess({required this.pids, required this.name, required this.memoryUsage, required this.cpuPercent, this.icon});
+  factory TaskmanagerProcess.fromMap(MapEntry<String, dynamic> data) => TaskmanagerProcess(pids: List<int>.from(data.value['pids'] ?? []), name: data.key, memoryUsage: data.value['memoryUsage']?.toDouble() ?? 0.0, cpuPercent: data.value['cpuPercent']?.toDouble() ?? 0.0, icon: data.value['icon'] != null ? base64Decode(data.value['icon']) : null);
+}
+
+class CpuInfo {
+  String name;
+  String socket;
+  int speed;
+  int cores;
+  int threads;
+  CpuInfo({required this.name, required this.socket, required this.speed, required this.cores, required this.threads});
+  factory CpuInfo.fromMap(Map<String, dynamic> map) => CpuInfo(name: map['name'] ?? '', socket: map['socket'] ?? '', speed: map['speed']?.toInt() ?? 0, cores: map['cores']?.toInt() ?? 0, threads: map['threads']?.toInt() ?? 0);
+}
+
+class NotificationData {
+  final NewNotificationKey? key;
+  final NotificationKeyWithUnit? childKey;
+  final NewNotificationFactorType? factorType;
+  final double? factorValue;
+  final int? secondsThreshold;
+  bool suspended;
+  NotificationData({this.key, this.childKey, this.factorType, this.factorValue, this.secondsThreshold, this.suspended = false});
+  factory NotificationData.fromMap(Map<String, dynamic> map) => NotificationData(key: map['key'] != null ? NewNotificationKey.values.byName(map['key']) : null, childKey: map['childKey'] != null ? NotificationKeyWithUnit.fromMap(map['childKey']) : null, factorType: map['factorType'] != null ? NewNotificationFactorType.values.byName(map['factorType']) : null, factorValue: double.parse(map['factorValue'].toString()), secondsThreshold: map['secondsThreshold']?.toInt(), suspended: map['suspended'] ?? false);
+}
+
+class NotificationKeyWithUnit {
+  String keyName;
+  String unit;
+  String? displayName;
+  NotificationKeyWithUnit({required this.keyName, required this.unit, this.displayName});
+  factory NotificationKeyWithUnit.fromMap(Map<String, dynamic> map) => NotificationKeyWithUnit(keyName: map['keyName'] ?? '', unit: map['unit'] ?? '', displayName: map['displayName']);
+  Map<String, dynamic> toMap() => {'keyName': keyName, 'unit': unit, 'displayName': displayName};
+}
+
+class FpsData {
+  List<double> fpsList;
+  double currentFps;
+  double averageFps;
+  double fps01Low;
+  double fps001Low;
+  FpsData({required this.fpsList, required this.currentFps, required this.fps01Low, required this.fps001Low, required this.averageFps});
+}
+
+class FpsRecord {
+  FpsData fpsData;
+  String presetName;
+  String presetDuration;
+  String? note;
+  FpsRecord({required this.fpsData, required this.presetName, required this.presetDuration, this.note});
+}
+
+class ComputerSpecs {
+  String motherboardName;
+  String ramSize;
+  String gpuName;
+  String cpuName;
+  List<String> storages;
+  List<String> monitors;
+  ComputerSpecs({required this.motherboardName, required this.ramSize, required this.gpuName, required this.cpuName, required this.storages, required this.monitors});
+  factory ComputerSpecs.fromComputerData(ComputerData data) => ComputerSpecs(motherboardName: data.motherboard.name, ramSize: "${(data.ram.memoryAvailable + data.ram.memoryUsed).toStringAsFixed(2)}GB", gpuName: data.gpu.name, cpuName: data.cpu.name, storages: data.storages.map((e) => e.type).toList(), monitors: data.monitors.map((e) => e.name).toList());
+  Map<String, dynamic> toMap() => {'motherboardName': motherboardName, 'ramSize': ramSize, 'gpuName': gpuName, 'cpuName': cpuName, 'storages': storages, 'monitors': monitors};
+}
+
+class ErrorParsingComputerData implements Exception {
+  final String data;
+  final Object error;
+  ErrorParsingComputerData(this.data, this.error);
+}
